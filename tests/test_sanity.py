4e535af1a0d27626727d1e8c1e7739eaf9cb97a3
Vinod Sharma <vinodsharma.mimit@gmail.com>
2018-02-24 00:44:25 +0530

    WIP: Adding a real function and testcase

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+try:
+    from src.dummy import dummy_func
+except ImportError:
+    raise Exception("src.dummy Import Error")
+
+
 def test_sanity():
     assert True is True
 
@@ -6,5 +16,9 @@ def test_sanity2():
     assert True is True
 
 
-if __name__ == "__main__":
-    test_sanity()
+def test_dummy_func():
+    eo = 1
+
+    o = dummy_func(True)
+
+    assert eo == o
